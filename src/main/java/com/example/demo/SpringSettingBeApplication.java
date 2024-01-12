305e0717cf76bb82301c8fcbc72a1dc518bd9c64
abt576 <limut0913@gmail.com>
2024-01-13 03:26:33 +0900

    server_two

---
+++ src/main/java/com/example/demo/SpringSettingBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSettingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSettingBeApplication.class, args);
+	}
+
+}
